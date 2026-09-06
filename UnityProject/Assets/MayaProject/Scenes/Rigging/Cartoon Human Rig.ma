29cb2c921757289f17a6dd833314d67fd9a386c6
Elizabeth Hart <sizzyhart@gmail.com>
2026-09-05 21:55:02 -0600

    Finished Broken FK

--- UnityProject/Assets/MayaProject/Scenes/Rigging/Cartoon Human Rig.ma
+++ UnityProject/Assets/MayaProject/Scenes/Rigging/Cartoon Human Rig.ma
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:570bd1b0e7b62ccaf2824118f407a00f8aa4ba54f55c0ef2b54482bd008a3115
-size 4680179
+oid sha256:82caf0b56b8b9405318ed3f318898c6e5f8280945a450337161a5a7d8bd1ff1e
+size 5045497
---
+++ UnityProject/Assets/MayaProject/Scripts/Broken_FK.py
@@ -0,0 +1,20 @@
+import maya.cmds as cmds
+
+sels = cmds.ls(sl=True)
+parent_ctrl = sels[0]
+child_ctrl = sels[1]
+
+child_ctrl_grp = cmds.listRelatives(child_ctrl, parent=True)[0]
+
+p_constraint1 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipRotate=['x','y','z'], weight=1)[0]
+p_constraint2 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipRotate=['x','y','z'], weight=1)[0]
+
+cmds.addAttr(child_ctrl, ln='FollowTranslate', at='double', min=0, max=1, dv=1)
+cmds.setAttr('%s.FollowTranslate' % (child_ctrl), e=True, Keyable=True)
+cmds.addAttr(child_ctrl, ln='FollowRotate', at='double', min=0, max=1, dv=1)
+cmds.setAttr('%s.FollowRotate' % (child_ctrl), e=True, Keyable=True)
+
+cmds.connectAttr('%s.FollowTranslate' % (child_ctrl), '%s.w0' % (p_constraint1), f=True
+cmds.connectAttr('%s.FollowRotate' % (child_ctrl), '%s.w0' % (p_constraint2), f=True
+
+
